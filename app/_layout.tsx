d0e042c96534e6ea5a561ab3a67aed794d8f9766
marksikaundi <emmanuelmark117@gmail.com>
2026-02-23 21:58:13 +0200

    kkk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,7 @@ export default function RootLayout() {
     <PapersProvider>
       <Stack
         screenOptions={{
+          headerShown: false,
           headerStyle: {
             backgroundColor: "#FFFFFF",
           },
